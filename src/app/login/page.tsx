cb1e03d1fb7f1150a07bf421cb39f599bf004316
azkalonz <aeartixenter@gmail.com>
2024-08-06 08:03:44 +0800

    nextjs migration initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,54 @@
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
+export default function LoginPage() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-[512px,1fr] xl:min-h-[800px]">
+      <div className="hidden bg-primary lg:flex flex-col">
+        <div className="w-full h-full flex items-center justify-center">
+          <img src="/images/cc-logo.png" alt="Credit Connection" width="365" height="140" />
+        </div>
+        <div className="w-full flex justify-center">
+          <img src="/images/girl-on-the-bike.png" width="278" height="267" />
+        </div>
+      </div>
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2">
+            <h1 className="text-3xl">Welcome to your</h1>
+            <h1 className="text-3xl font-bold text-accent">Mortgage Action Plan</h1>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" required />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link href="#" className="ml-auto inline-block text-sm underline" prefetch={false}>
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{' '}
+            <Link href="#" className="underline" prefetch={false}>
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
