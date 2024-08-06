375934f612232bd6839c665c284ab67e0e7c94c4
azkalonz <aeartixenter@gmail.com>
2024-08-06 08:02:45 +0800

    add client page

--- app/src/pages/admins-and-staffs/add-client/AS_AddClientPage.tsx
+++ app/src/pages/admins-and-staffs/add-client/AS_AddClientPage.tsx
@@ -1,9 +1,11 @@
+import InputWithLabel from "../../../components/molecules/input-with-label/InputWithLabel";
 import DashboardTemplate from "../../../components/templates/dashboard/default/DashboardTemplate";
 
 function AS_AddClientPage() {
   return (
     <DashboardTemplate>
       <h1>Add New Client</h1>
+      <InputWithLabel label="Testing" />
     </DashboardTemplate>
   );
 }
