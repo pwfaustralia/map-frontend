b1d079a5966215dc489770e5a4d67a7369cd5333
azkalonz <aeartixenter@gmail.com>
2024-08-08 10:28:28 +0800

    feat: email/password auth provider

---
+++ src/app/api/auth/credentials/route.ts
@@ -0,0 +1,17 @@
+import { fetchLaravel } from '@/lib/fetcher';
+import { EmailPasswordSchema } from '@/lib/schema/auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (request: NextRequest) => {
+  const credentials = await request.json();
+  const validation = EmailPasswordSchema.safeParse(credentials);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.flatten(), { status: 422 });
+  }
+  const login = await fetchLaravel('/users/login', {
+    method: 'post',
+    body: JSON.stringify(validation.data),
+  }).then((response) => response.json());
+
+  return NextResponse.json(login);
+};
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,60 @@
 'use client';
 
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import Link from 'next/link';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
-import { signIn } from 'next-auth/react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { NEXT_APP_ROUTES } from '@/lib/routes';
+import { EmailPasswordSchema } from '@/lib/schema/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { InfoCircledIcon } from '@radix-ui/react-icons';
+import { signIn, signOut } from 'next-auth/react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 export default function LoginPage() {
+  const {
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    register,
+    setError,
+  } = useForm({
+    defaultValues: {
+      email: 'hello@pwf.com.au',
+      password: 'LK^3gxs8!!8&hu',
+    },
+    resolver: zodResolver(EmailPasswordSchema),
+  });
+  const [otherError, setOtherError] = useState('');
+  const router = useRouter();
+
+  const handleSignIn = async (credentials: z.infer<typeof EmailPasswordSchema>) => {
+    setOtherError('');
+    const signinReq = await signIn('email-password', {
+      ...credentials,
+      redirect: false,
+    });
+    if (signinReq?.error) {
+      try {
+        const errors = JSON.parse(signinReq.error);
+        errors?.forEach((err: any) => {
+          setError(err.path[0], err);
+        });
+      } catch (e) {
+        if (signinReq.error) {
+          setOtherError(signinReq.error);
+        }
+      }
+    } else {
+      if (!signinReq?.error && signinReq?.url) {
+        router.replace(NEXT_APP_ROUTES.dashboard);
+      }
+    }
+  };
+
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-[512px,1fr] xl:min-h-[800px]">
       <div className="hidden bg-primary lg:flex flex-col">
@@ -23,33 +71,41 @@ export default function LoginPage() {
             <h1 className="text-3xl">Welcome to your</h1>
             <h1 className="text-3xl font-bold text-accent">Mortgage Action Plan</h1>
           </div>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" required />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link href="#" className="ml-auto inline-block text-sm underline" prefetch={false}>
-                  Forgot your password?
-                </Link>
+          <form onSubmit={handleSubmit((d) => handleSignIn(d as z.infer<typeof EmailPasswordSchema>))}>
+            <div className="grid gap-4">
+              {otherError && (
+                <Alert variant="destructive">
+                  <InfoCircledIcon />
+                  <AlertTitle>Login failed</AlertTitle>
+                  <AlertDescription>{otherError}</AlertDescription>
+                </Alert>
+              )}
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input {...register('email')} disabled={isSubmitting} error={errors.email?.message} type="email" />
               </div>
-              <Input id="password" type="password" required />
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <Link href="#" className="ml-auto inline-block text-sm underline" prefetch={false}>
+                    Forgot your password?
+                  </Link>
+                </div>
+                <Input
+                  {...register('password')}
+                  disabled={isSubmitting}
+                  error={errors.password?.message}
+                  type="password"
+                />
+              </div>
+              <Button type="submit" className="w-full" disabled={isSubmitting}>
+                Login
+              </Button>
+              <Button variant="outline" className="w-full" disabled={isSubmitting}>
+                Login with Google
+              </Button>
             </div>
-            <Button
-              type="submit"
-              className="w-full"
-              onClick={() => {
-                signIn('laravel-passport');
-              }}
-            >
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </div>
+          </form>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{' '}
             <Link href="#" className="underline" prefetch={false}>
